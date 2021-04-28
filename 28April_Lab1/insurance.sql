1a28e56991fff0f4fe2f1ecb392bddafbe9fd001
KIRAN-MK-211 <67577151+KIRAN-MK-211@users.noreply.github.com>
2021-04-28 15:49:04 +0530

    Create insurance.sql

---
+++ 28April_Lab1/insurance.sql
@@ -0,0 +1,34 @@
+create database insurance;
+use insurance;
+CREATE TABLE PERSON (DRIVER_ID VARCHAR(10),NAME VARCHAR(20),ADDRESS
+VARCHAR(15),PRIMARY KEY(DRIVER_ID));
+create table car(regno varchar(10),Model varchar(20),Year date,Primary key(Regno));
+create table Accident(report_no int,ADATE DATE,Location varchar(15),Primary
+key(report_no));
+
+create table owns(driver_id varchar(10),regno varchar(10),primary key(driver_id,regno),
+foreign key(driver_id) references person(driver_id) on delete cascade,
+foreign key(regno) references car(regno) on delete cascade);
+
+CREATE TABLE PARTICIPATED(driver_id varchar(10),regno varchar(10),report_no int,
+damage_amt float, foreign key (driver_id,regno) references OWNS(driver_id,regno)
+ON DELETE CASCADE,
+foreign key (REPORT_NO) references ACCIDENT(REPORT_NO) ON DELETE CASCADE);
+
+use insurance;
+INSERT INTO PERSON VALUES("1111","RAMU","K.S.LAYOUT");
+INSERT INTO PERSON VALUES("2222","JOHN","INDIRANAGAR"),("3333","PRIYA","JAYNAGAR"), ("4444","GOPAL","WHITEFIELD"),("5555","LATHA","VIJAYNAGAR");
+
+INSERT INTO CAR VALUES("KA04Q2301","MARUTHI-DX","2000-01-01"),("KA05P1000","FORDICON","2000-01-01"),("KA03L1234","ZEN-VXI","1999-01-01"),("KA03L9999","MARUTHI-DX","2002-01-01"),("KA01P4020","INDICA-VX","2002-01-01");
+
+INSERT INTO ACCIDENT VALUES(12,"2002-06-01","M G ROAD"),(200,"2002-12-10","DOUBLE ROAD"),(300,"1999-07-23","M G ROAD"),(25000,"2000-06-11","RESIDENCY ROAD"),(26500,"2001-10-16","RICHMOND ROAD");
+
+INSERT INTO OWNS VALUES("1111","KA04Q2301"),("1111","KA05P1000"),("2222","KA03L1234"),("3333","KA03L9999"),("4444","KA01P4020");
+
+INSERT INTO PARTICIPATED VALUES("1111","KA04Q2301",12,20000),("2222","KA03L1234",200,500),("3333","KA03L9999",300,10000),("4444","KA01P4020",25000,2375),("1111","KA05P1000",26500,70000);
+
+UPDATE PARTICIPATED SET DAMAGE_AMT=25000 WHERE REPORT_NO=12 AND REGNO="KA04Q2301";
+
+SELECT COUNT(*) FROM ACCIDENT WHERE ADATE LIKE "2002-__-__";
+
+SELECT COUNT(A.REPORT_NO) FROM ACCIDENT A,PARTICIPATED P,CAR C WHERE A.REPORT_NO = P.REPORT_NO AND P.REGNO=C.REGNO AND C.MODEL="MARUTHI-DX";
